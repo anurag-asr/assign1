dba65837b4082529300d5b344b59c0b268ce3216
anurag-asr <anurjwt@gmail.com>
2022-05-19 09:02:39 +0530

    ss

---
+++ src/components/Assign.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Assign = () => {
+  return (
+    <div>
+
+      <h1> MObile Operatin System</h1>
+      <p>*Android</p>
+      <p>*Blackberry</p>
+      <p>*Iphone</p>
+      <p>*Window</p>
+
+      <h1> MObile Manufacturer</h1>
+      <p>*Samsung</p>
+      <p>*Htc</p>
+      <p>*Micromax</p>
+      <p>*Apple</p>
+
+    </div>
+  )
+}
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/anurag-asr/assign1.git
+// git push -u origin main
+
+export default Assign
